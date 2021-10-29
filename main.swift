acc27f9051caa11a8f08af5e1122db68cf564190
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-10-29 10:47:46 +0100

    Guard Statements

--- main.swift
+++ main.swift
@@ -7,24 +7,28 @@
 
 import Foundation
 
-// Conditionals
+// Guard Statements
 
-var x = 19
-
-if x > 20 {
-    
-} else if x > 10 {
-    
-} else {
-    // Something else
-}
-
-var y: Int? = nil
-
-if let value = y {
-    // Use value in here
-    
-    if x > 10 && value < 100 {
-        
+func numberLargerThanFive(number: Int) -> Bool {
+    // If number is greater than 5 continue to the rest of the function
+    guard number > 5, number > 4 else {
+        return false
     }
+    return true
 }
+
+var text: String? // nil by default
+text = "hello world"
+
+if let value = text {
+    print(value)
+}
+
+func printValue() {
+    guard let value = text else {
+        return
+    }
+    print(value)
+}
+
+printValue()
