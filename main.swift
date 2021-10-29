d7fcbfc39fd8d9d07b85a439bc0cd78602af515c
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-10-29 09:25:21 +0100

    Variable, Constant, Static

--- main.swift
+++ main.swift
@@ -7,4 +7,22 @@
 
 import Foundation
 
+// Variable, Constant, Static
+
+var str = "Hello, World!"
+var x = 4
+var y = 1
+
+y = 2
+
+let z = 3
+// z = 12 // Cannot do this! this is a constant
+
+// Static constants
+class Person {
+    static let dave = Person() // Static constant
+}
+
+var john = Person()
+let dave = Person.dave
 
