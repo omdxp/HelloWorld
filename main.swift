7aebb3fa547bf9c6b4511b7c810589685c4c7b60
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-10-29 14:15:06 +0100

    Frameworks and Libraries

--- main.swift
+++ main.swift
@@ -6,17 +6,9 @@
 //
 
 import Foundation
+import StoreKit
+import UIKit
 
-// Numbers
+// Frameworks and Libraries
 
-var a: Int = 1 // Int
-var b: Double = 1.00 // Double
-var c: Float = 1.0000000000001 // Float
-
-var result = a + Int(b)
-var result2 = Double(a) + b
-
-var x: Int? = 1
-if let value = x {
-    print(value)
-}
+let button = UIButton()
